ce91eb50ea0069ddce09150322e446f697c893cc
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-08 09:18:30 -0600

    Moves author_list method to book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Book, type: :model do
     it {should validate_presence_of :year_published}
     it {should validate_presence_of :thumbnail_url}
   end
+
+  describe 'instance methods' do
+    describe '#author_list' do
+      it "should return full author list" do
+        hunger = Book.create(title: 'The Hunger Games', page_count: 374, year_published: 2008, thumbnail_url: 'https://images.gr-assets.com/books/1447303603l/2767052.jpg')
+        hunger.authors << Author.find_or_create_by(name: 'Suzanne Collins')
+        hunger.authors << Author.find_or_create_by(name: 'Carrie Walsh')
+
+        expected = "Suzanne Collins, Carrie Walsh"
+        actual = hunger.author_list
+
+        expect(expected).to eq(actual)
+      end
+    end
+  end 
 end
