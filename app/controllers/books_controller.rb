9b63f4143f5e74e1df3094556c75362a3c80eb3c
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-09 10:38:40 -0600

    New book Creation, need to add sad path
    Co-authored-by: Glynnis O'Connell <44204866+GlynnisOC@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,21 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    # @author = Author.new(params[:book][:author])
+    # @author.save
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :page_count, :year_published, :thumbnail_url, authors_attributes: [:name])
+  end
 end
---
+++ spec/features/new_book_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "book creation workflow" do
+  it "allows user to create new book" do
+    visit new_book_path
+
+    fill_in 'book[title]', with: "Life Will Be the Death of Me"
+    fill_in 'book[page_count]', with: 325
+    fill_in 'book[year_published]', with: "2019"
+    fill_in "Author(s)", with: "Chelsea Handler"
+    fill_in 'book[thumbnail_url]', with: "google.com"
+    click_on "Add Book"
+    expect(current_path).to eq(books_path)
+
+    new_book = Book.find_by(title: "Life Will Be the Death of Me")
+    new_author = new_book.authors.last
+
+    expect(page).to have_content(new_book.title)
+    expect(page).to have_content(new_book.page_count)
+    expect(page).to have_content(new_book.year_published)
+    expect(page).to have_content(new_author.name)
+  end
+end
