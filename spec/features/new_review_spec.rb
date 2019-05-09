446e40ec71bc64baf28fa69175c6d1c1e8200ebd
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-09 13:52:02 -0600

    Add form for review
    Co-authored-by: Glynnis O'Connell <44204866+GlynnisOC@users.noreply.github.com>

---
+++ spec/features/new_review_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "new review workflow" do
+  it "allows user to add review" do
+    king = Book.create(title: 'The Return Of The King', page_count: 416, year_published: 1955, thumbnail_url: 'https://images-na.ssl-images-amazon.com/images/I/41Qx%2BidkxsL.jpg')
+    visit new_book_review_path(king)
+
+    fill_in 'review[user]', with: "Brian"
+    fill_in 'review[review_headline]', with: "Great read"
+    fill_in 'review[review_score]', with: 4
+    fill_in 'review[review_body]', with: "It was great."
+    click_on "Add Review"
+
+    review = king.reviews.last
+    save_and_open_page
+    within(".reviews") do
+      within("#id-#{review.id}") do
+        expect(page).to have_content(review.user)
+        expect(page).to have_content(review.review_headline)
+        expect(page).to have_content(review.review_score)
+        expect(page).to have_content(review.review_body)
+      end
+    end
+  end
+end
